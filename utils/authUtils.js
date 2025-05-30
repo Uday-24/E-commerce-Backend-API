ce71f89fb25b09d6164135fba7fda43501d1e783
Uday-24 <chotaliyauday24@gmail.com>
2025-05-30 18:08:31 +0530

    More effecient code added

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 exports.signAccessToken = (user) => {
-    return jwt.sign({ userId: user.id, userRole: user.role }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_EXPIRE});
+    return jwt.sign({ userId: user.id, userRole: user.role }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_EXPIRE || '15m' });
 }
 
 exports.signRefreshToken = (user) => {
-    return jwt.sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET, {expiresIn: process.env.REFRESH_EXPIRE});
+    return jwt.sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET, {expiresIn: process.env.REFRESH_EXPIRE || '7d'});
 }
